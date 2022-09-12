90ba68b677358b93c1a79424e07835ec9b572a07
liuhanhao <272859963@qq.com>
2022-09-12 21:20:20 +0800

    22

---
+++ hello/Sources/App/Controllers/EmailService.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by 刘汉浩 on 2022/9/12.
+//
+
+import Foundation
+
+/*
+ Hedwig 是一个 Swift 包，提供了一组高级 API，可以让你轻松地发送电子邮件到SMTP服务器。如果你打算在下一个神奇的 Swift 服务器 app里发送 email，Hedwig 可能是一个好的选择。
+ 
+ */
